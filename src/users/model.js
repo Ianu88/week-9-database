1c7c6c4bc8ea931f75f5e49636b8be95a3deb484
IanUpton <uptonian22@gmail.com>
2024-09-23 14:51:15 +0100

    enhancement: add User model

---
+++ src/users/model.js
@@ -0,0 +1,23 @@
+const{DataTypes} = require("sequelize");
+const sequelize = require("../DB/connections");
+
+const User = sequelize.define(
+   
+    "user",
+    {
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        email:{
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+    module.exports = User
