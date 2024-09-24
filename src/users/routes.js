4975c90d4e1c19d5b59c7ee38d9430dde1aa2542
IanUpton <uptonian22@gmail.com>
2024-09-24 11:12:15 +0100

    enhancement: add hashPass function

--- src/users/routes.js
+++ src/users/routes.js
@@ -1,9 +1,9 @@
 const {Router} = require("express");
 const userRouter = Router();        
-
+const {hashPass} = require("../middleware/auth")
 const {addUser} = require("./controllers")
 
-userRouter.post("/users/signup", addUser);
+userRouter.post("/users/signup", hashPass, addUser);
 
 
 module.exports = userRouter;
